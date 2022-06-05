ecd0658c0a6b6781c58c65e9f713a6a53f4ab3c5
Tabrej <tabrejhussain7@gmail.com>
2022-06-05 21:40:49 +0530

    ss

--- helper.java
+++ helper.java
@@ -1,15 +1,32 @@
 import javax.swing.*;
-class helper{
+class helper extends JFrame{
+   
+    //second method by   
+    
+    // helper(){
+            
+    //         setSize(400,500);
+    //         setVisible(true);
+            
+    //     }
+
+
+
+
     public static void main(String[] args) {
         // System.out.println("Hello");
         // System.out.println("Bye");
 
         //1 creating frame 
-        JFrame frame=new JFrame();
-        frame.setSize(400, 500);
-        frame.setLayout(null);
-        frame.setLocationRelativeTo(null);//to the center of the screen
-        frame.setVisible(true);
+        // JFrame frame=new JFrame();
+        // frame.setSize(400, 500);
+        // frame.setLayout(null);
+        // frame.setLocationRelativeTo(null);//to the center of the screen
+        // frame.setVisible(true);
+
+        // new helper();
+
+        
 
 
 
