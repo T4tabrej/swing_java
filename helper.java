42cdd70d2493781f3669301acbbc495e9e436b13
Tabrej <tabrejhussain7@gmail.com>
2022-06-05 21:25:07 +0530

    sss

--- helper.java
+++ helper.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
 class helper{
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Bye");
+        // System.out.println("Hello");
+        // System.out.println("Bye");
+
+        //1 creating frame 
+        JFrame frame=new JFrame();
+        frame.setSize(400, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+
+
+
     }
 }
